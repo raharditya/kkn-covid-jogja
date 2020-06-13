41f38c46fc1f5ddf58bbc2e4f499fd22f999edad
Asebodi <aditraharditya45@gmail.com>
2020-06-13 19:27:42 +0700

    Added all news sources to Berita model

--- models/Berita.model.js
+++ models/Berita.model.js
@@ -5,6 +5,56 @@ const Berita = mongoose.Schema({
       {
         title: String,
         excerpt: String,
+        url: String,
+        thumbnail: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    suara: [
+      {
+        title: String,
+        excerpt: String,
+        url: String,
+        thumbnail: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    tribunnews: [
+      {
+        title: String,
+        excerpt: String,
+        source: String,
+        url: String,
+        thumbnail: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    kompas: [
+      {
+        title: String,
+        excerpt: String,
+        source: String,
+        url: String,
+        thumbnail: String,
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    cnbc: [
+      {
+        title: String,
+        excerpt: String,
         source: String,
         url: String,
         thumbnail: String,
