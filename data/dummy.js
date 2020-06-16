6b398660556432984de5ecfa3175fecc02fa7a51
Asebodi <aditraharditya45@gmail.com>
2020-06-16 23:56:05 +0700

    Added dummy data for testing

---
+++ data/dummy.js
@@ -0,0 +1,18 @@
+const data = {
+  jenisWilayah: "Kabupaten",
+  namaWilayah: "Bantul",
+  kecamatan: [
+    {
+      id: 1,
+      namaKecamatan: "Bambang Lipuro",
+      kodePos: 55764,
+    },
+    {
+      id: 2,
+      namaKecamatan: "Banguntapan",
+      kodePos: 55198,
+    },
+  ],
+};
+
+module.exports = data;
