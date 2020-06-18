6cce05b625597055ba7b322c2615a93f4420a17f
Asebodi <aditraharditya45@gmail.com>
2020-06-18 22:27:51 +0700

    Connected to database

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const App = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4500;
+
+(async function dbConnect() {
+  try {
+    mongoose.connect(
+      process.env.DB_SECRET,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(`DB Connected!`)
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
 
 const berita = require("./routes/api/Berita");
 
 App.use("/", berita);
+
+App.listen(PORT, () => console.log(`Server started at ${PORT}`));
