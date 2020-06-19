77a80a4a09d7371445938aaa0dc4f3bbf9145daa
Asebodi <aditraharditya45@gmail.com>
2020-06-19 09:48:03 +0700

    changed Berita model to the correct ways of storing documents

--- models/Berita.model.js
+++ models/Berita.model.js
@@ -1,51 +1,16 @@
 const mongoose = require("mongoose");
 const Berita = mongoose.Schema({
-  source: {
-    detik: [
-      {
-        title: String,
-        date: String,
-        excerpt: String,
-        url: String,
-        thumbnail: String,
-      },
-    ],
-    suara: [
-      {
-        title: String,
-        excerpt: String,
-        url: String,
-        thumbnail: String,
-      },
-    ],
-    tribunnews: [
-      {
-        title: String,
-        date: String,
-        excerpt: String,
-        url: String,
-        thumbnail: String,
-      },
-    ],
-    kompas: [
-      {
-        title: String,
-        date: String,
-        excerpt: String,
-        url: String,
-        thumbnail: String,
-      },
-    ],
-    cnbc: [
-      {
-        title: String,
-        date: String,
-        label: String,
-        url: String,
-        thumbnail: String,
-      },
-    ],
-  },
+  source: String,
+  news: [
+    {
+      title: String,
+      date: String,
+      label: String,
+      excerpt: String,
+      url: String,
+      thumbnail: String,
+    },
+  ],
   date: {
     type: Date,
     default: Date.now,
