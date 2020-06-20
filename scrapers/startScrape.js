cbdad09f0c2d6baca08072d0de27d7428498795d
Asebodi <aditraharditya45@gmail.com>
2020-06-20 19:41:42 +0700

    Imported all news scraper and handled initiation

--- scrapers/startScrape.js
+++ scrapers/startScrape.js
@@ -1,9 +1,17 @@
 const Berita = require("../models/Berita.model");
 
 const detikNews = require("./detikScraper");
+const cnbcNews = require("./cnbcScraper");
+const kompasNews = require("./kompasScraper");
+const suaraNews = require("./suaraScraper");
+const tribunNews = require("./tribunScraper");
 
 async function startScrape() {
   detikNews();
+  cnbcNews();
+  kompasNews();
+  suaraNews();
+  tribunNews();
 }
 
 module.exports = startScrape;
