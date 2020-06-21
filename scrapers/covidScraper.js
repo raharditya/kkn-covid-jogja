5bdf1f4e72b84f30fc16eff869e89a70325c943f
Asebodi <aditraharditya45@gmail.com>
2020-06-21 13:12:30 +0700

    Successful scrape through all postal for covid data

--- scrapers/covidScraper.js
+++ scrapers/covidScraper.js
@@ -2,41 +2,56 @@ const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
 const bantulPostal = require("../data/bantul-postal.json");
-// const data = JSON.parse(bantulPostal);
+const gunungkidulPostal = require("../data/gunungkidul-postal.json");
+const kulonprogoPostal = require("../data/kulonprogo-postal.json");
+const slemanPostal = require("../data/sleman-postal.json");
+const yogyakartaPostal = require("../data/yogyakarta-postal.json");
+
+const scrapeAll = [
+  bantulPostal,
+  gunungkidulPostal,
+  kulonprogoPostal,
+  slemanPostal,
+  yogyakartaPostal,
+];
 
 async function scrape() {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
 
-  for (let i = 0; i < bantulPostal.kecamatan.length; i++) {
-    const page = await browser.newPage();
-    await page.goto("https://sebaran-covid19.jogjaprov.go.id/kodepos");
-    await page.type("#fname", bantulPostal.kecamatan[i].kodePos.toString());
+  for (let curr = 0; curr < scrapeAll.length; curr++) {
+    for (let i = 0; i < scrapeAll[curr].kecamatan.length; i++) {
+      const page = await browser.newPage();
+      await page.goto("https://sebaran-covid19.jogjaprov.go.id/kodepos");
+      await page.type(
+        "#fname",
+        scrapeAll[curr].kecamatan[i].kodePos.toString()
+      );
 
-    await Promise.all([
-      page.click(".btn"),
-      page.waitForNavigation({ waitUntil: "networkidle0" }),
-    ]);
+      await Promise.all([
+        page.click(".btn"),
+        page.waitForNavigation({ waitUntil: "networkidle0" }),
+      ]);
 
-    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+      let bodyHTML = await page.evaluate(() => document.body.innerHTML);
 
-    const $ = cheerio.load(bodyHTML);
+      const $ = cheerio.load(bodyHTML);
 
-    const positif = $("#positif").text();
-    const odp = $("#odp").text();
-    const pdp = $("#pdp").text();
+      const positif = $("#positif").text();
+      const odp = $("#odp").text();
+      const pdp = $("#pdp").text();
 
-    const push = {
-      kecamatan: bantulPostal.kecamatan[i].namaKecamatan,
-      positif,
-      odp,
-      pdp,
-    };
+      const push = {
+        kecamatan: scrapeAll[curr].kecamatan[i].namaKecamatan,
+        positif,
+        odp,
+        pdp,
+      };
 
-    console.log(push);
-    // console.log(bantulPostal.nama);
-    //   await page.screenshot({ path: "kode-pos.png" });
-    await page.close();
+      console.log(push);
+      await page.close();
+    }
   }
+
   await browser.close();
 }
 
