efe90f2d0c24dcf709bd688c82cce0ee6931486c
Asebodi <aditraharditya45@gmail.com>
2020-07-03 15:28:35 +0700

    Impelented back button in Article and Subpage header

--- client/src/components/ArticleHeader.jsx
+++ client/src/components/ArticleHeader.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { ReactComponent as LeftArrow } from "../assets/left-arrow.svg";
+import { Link } from "react-router-dom";
 
 function ArticleHeader(props) {
   return (
     <header style={{ backgroundImage: `url(${props.img})` }}>
       <div className="page-inner-wrapper">
         <div className="btn-container">
-          <div className="back-btn">
-            <div className="btn-inner">
-              <LeftArrow />
+          <Link to="/edukasi">
+            <div className="back-btn">
+              <div className="btn-inner">
+                <LeftArrow />
+              </div>
             </div>
-          </div>
+          </Link>
         </div>
 
         <h2 className="article-title">{props.title}</h2>
--- client/src/components/SubpageHeader.jsx
+++ client/src/components/SubpageHeader.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import { ReactComponent as LeftArrow } from "../assets/left-arrow.svg";
+import { Link } from "react-router-dom";
 
 function SubpageHeader(props) {
   return (
     <header>
       <div className="page-inner-wrapper">
-        <div className="back-btn">
-          <div className="btn-inner">
-            <LeftArrow />
+        <Link to="/berita">
+          <div className="back-btn">
+            <div className="btn-inner">
+              <LeftArrow />
+            </div>
           </div>
-        </div>
+        </Link>
 
         <h2 className="page-title">{props.title}</h2>
         <p className="page-subtitle">{props.subtitle}</p>
