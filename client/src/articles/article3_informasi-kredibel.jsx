42800cc395cae8a2065a89aa7ff979f594ebb701
Asebodi <aditraharditya45@gmail.com>
2020-08-06 21:48:40 +0700

    Added new article

---
+++ client/src/articles/article3_informasi-kredibel.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Thumb from "./article-thumbs/article3.jpg";
+
+const content = (
+  <>
+    <p>
+      Di era modern ini, informasi sangat mudah untuk tersebar di masyarakat.
+      Meskipun ini menunjukkan perkembangan yang positif, teknologi ini juga
+      bisa dipakai untuk menyebarkan misinformasi atau informasi yang salah dan
+      menyesatkan.
+    </p>
+    <p>
+      Di artikel ini tim KKN Covid UNS Jogja sudah mengumpulkan berbagai tips
+      agar kita semua dapat mem-filter mana informasi yang tidak benar dan mana
+      yang kredibel.
+    </p>
+    <p>
+      <strong>1. Perhatikan sumber</strong>
+      <br />
+      Hal pertama yang wajib dilakukan ketika menerima suatu informasi adalah
+      melihat siapa yang berbicara dibalik informasi tersebut. Hal ini dapat
+      berupa latar belakang individual dan afiliasi pekerjaannya, ataupun
+      organisasi dibalik munculnya informasi tersebut.
+    </p>
+    <p>
+      <strong>2. Selalu waspada</strong>
+      <br />
+      Kita harus bersifat selalu waspada terhadap segala informasi yang didapat
+      dari Whatsapp, Instagram, Youtube, maupun media lainnya, terutama jika
+      sumber informasi tersebut tidak jelas. Seperti pada tips nomor satu,
+      pastikan channel atau akun yang membuat informasi tersebut kredibel.
+    </p>
+    <p>
+      <strong>3. Cari informasi lain</strong>
+      <br />
+      Ketika mendapatkan suatu informasi, kita juga harus double-check dengan
+      melihat informasi lainnya yang mendukung (atau menolak) informasi yang
+      kita dapatkan tadi. Dengan ini kita bisa tahu seberapa kredibel informasi
+      yang kita dapatkan.
+    </p>
+    <p>
+      <strong>4. Sumber terpercaya</strong>
+      <br />
+      Hal yang paling aman ketika mencari informasi adalah dengan mencarinya di
+      sumber-sumber yang terpercaya. Ini dapat berupa website satgas covid, atau
+      media online lainnya yang kredibel. Tidak hanya media online, kita juga
+      dapat menelpon hotline Covid-19 jika ingin menanyakan suatu informasi
+      untuk menkonfirmasinya secara resmi.
+    </p>
+
+    <p>
+      Jadi itu adalah 4 tips dari kami agar kita bisa mendapatkan dan memfilter
+      informasi yang kredibel. Selalu stay safe dan selalu gunakan masker!
+    </p>
+  </>
+);
+
+const articleMeta = {
+  title: "Tips Untuk Mendapatkan Informasi Kredibel",
+  url: "/edukasi/tips-untuk-mendapatkan-informasi-kredibel",
+  content: content,
+  img: Thumb,
+};
+
+export default articleMeta;
--- client/src/articles/combineArticles.jsx
+++ client/src/articles/combineArticles.jsx
@@ -1,6 +1,7 @@
 import article1 from "./article1_apa-itu-new-normal";
 import article2 from "./article2_new-normal-di-tempat-ibadah";
+import article3 from "./article3_informasi-kredibel";
 
-const articles = [article2, article1];
+const articles = [article3, article2, article1];
 
 export default articles;
