079bb082701c1b8a4a734ea96eba253f5105cfe7
Asebodi <aditraharditya45@gmail.com>
2020-08-11 17:33:16 +0700

    Possible bug fix for daily province fetch

--- scrapers/covidProvFetch.js
+++ scrapers/covidProvFetch.js
@@ -1,7 +1,10 @@
 const request = require("request");
 const CovidProvModel = require("../models/CovidProv.model");
 
+let dailyPush = {};
+
 const covidProvFetch = () => {
+  console.log(dailyPush);
   request(
     "https://indonesia-covid-19.mathdro.id/api/provinsi",
     async (err, res, html) => {
@@ -33,15 +36,13 @@ const covidProvFetch = () => {
         const now = new Date();
         const date = `${now.getDate()} ${monthName[now.getMonth()]} 20`;
 
-        let dailyPush = {
+        dailyPush = {
           activeDaily: provData.kasusPosi,
           recoveredDaily: provData.kasusSemb,
           deathDaily: provData.kasusMeni,
           patientDaily,
           date,
         };
-
-        console.log(dailyPush);
 
         try {
           let covidData = await CovidProvModel.findOne({ nameProv: "DIY" });
@@ -57,6 +58,7 @@ const covidProvFetch = () => {
 
             await covidData.save();
             dailyPush = {};
+            console.log(dailyPush);
           } else {
             covidData.activeProv = provData.kasusPosi;
             covidData.recoveredProv = provData.kasusSemb;
@@ -66,10 +68,12 @@ const covidProvFetch = () => {
             await covidData.save();
             console.log("Covid Prov data updated to DB");
             dailyPush = {};
+            console.log(dailyPush);
           }
         } catch (err) {
           console.error(err);
           dailyPush = {};
+          console.log(dailyPush);
         }
       }
     }
